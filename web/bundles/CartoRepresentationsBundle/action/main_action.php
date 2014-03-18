bacd618ba3923d9b08e61e61de2abf9956050b29
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-03-18 10:44:51 +0100

    à la demande de Juliana

--- web/bundles/CartoRepresentationsBundle/action/main_action.php
+++ web/bundles/CartoRepresentationsBundle/action/main_action.php
@@ -1,5 +1,14 @@
 <?php
-	$cmd = '';
+	$curlSession = curl_init();
+		curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/bundles/CartoRepresentationsBundle/json/exemple.json');
+		curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+		$jsonresult = curl_exec($curlSession);
+		curl_close($curlSession);
+
+	echo 'ça a marché';
+	/*$cmd = '';
 	if (isset($_POST['cmd'])){
 		$cmd = $_POST['cmd'];
 	}
@@ -24,5 +33,5 @@
 		curl_close($curlSession);
 				
 		exit('{"success":"true","data":'.$jsonresult.'}');
-	}
+	}*/
 ?>
