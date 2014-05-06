55e7b4e9d09bd88b9d538a38f888c15a6d665b01
seb <seb@seb.fr>
2014-05-06 22:29:08 +0200

    maj

--- web/bundles/CartoRepresentationsBundle/action/main_action.php
+++ web/bundles/CartoRepresentationsBundle/action/main_action.php
@@ -8,8 +8,8 @@
 	$curlSession = curl_init();
 
 	//Pour tester la possibilité de rechercher une ou des relations données, remplacez /all par /rel1,rel2,... (au moins une relation)
-	curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/fr/donnees/json/'.$cmd.'/all'); //Céline  // .'/'.$options = rel1,rel2,rel3
-	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/CartoSavoie/carto/web/fr/donnees/json/'.$cmd.'/all'); //Juliana
+	//curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/fr/donnees/json/'.$cmd.'/all'); //Céline  // .'/'.$options = rel1,rel2,rel3
+	curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/CartoSavoie/carto/web/fr/donnees/json/'.$cmd.'/all'); //Juliana
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/Projet%20-%20Visualisation%20de%20donnees/carto/web/fr/donnees/json/'.$cmd.'/all'); // Anthony
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://carto.dev/fr/donnees/json/'.$cmd.'/all'); // Anthony2
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://carto.dev/fr/donnees/json/'.$cmd.'/all');
