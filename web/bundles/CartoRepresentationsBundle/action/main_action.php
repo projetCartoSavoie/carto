b8598b7eab96a09994437fb372d0eb7415c51b20
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-05-07 08:42:59 +0200

    maj

--- web/bundles/CartoRepresentationsBundle/action/main_action.php
+++ web/bundles/CartoRepresentationsBundle/action/main_action.php
@@ -1,14 +1,15 @@
 <?php
 	if (isset($_POST['search'])){
 		$cmd = $_POST['search'];
-		//$options = //liste des relations à prendre en compte
+		//$options = //liste des relations à prendre en compte (all si aucune relation sélectionnée)
+		$options = 'all';
 	}
 	else { $cmd = 'entity'; }
 
 	$curlSession = curl_init();
 
 	//Pour tester la possibilité de rechercher une ou des relations données, remplacez /all par /rel1,rel2,... (au moins une relation)
-	curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/fr/donnees/json/'.$cmd.'/all'); //Céline  // .'/'.$options = rel1,rel2,rel3
+	curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/fr/donnees/json/'.$cmd.'/'.$options); //Céline  // .'/'.$options = rel1,rel2,rel3
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/CartoSavoie/carto/web/fr/donnees/json/'.$cmd.'/all'); //Juliana
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/Projet%20-%20Visualisation%20de%20donnees/carto/web/fr/donnees/json/'.$cmd.'/all'); // Anthony
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://carto.dev/fr/donnees/json/'.$cmd.'/all'); // Anthony2
