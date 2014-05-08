e596e79ef214bcc81d5787467fdb34ee4b7cd72b
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-05-08 08:15:26 +0200

    déplacement du css utils

--- web/bundles/CartoRepresentationsBundle/action/main_action.php
+++ web/bundles/CartoRepresentationsBundle/action/main_action.php
@@ -13,7 +13,6 @@
 
 	$curlSession = curl_init();
 
-	//Pour tester la possibilité de rechercher une ou des relations données, remplacez /all par /rel1,rel2,... (au moins une relation)
 	curl_setopt($curlSession, CURLOPT_URL, 'http://carto.localhost/fr/donnees/json/'.$cmd.'/'.$relations.'/'.$profondeur); //Céline  // .'/'.profondeur = 3
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/CartoSavoie/carto/web/fr/donnees/json/'.$cmd.'/'.$relations.'/'.$profondeur); //Juliana
 	//curl_setopt($curlSession, CURLOPT_URL, 'http://localhost/Projet%20-%20Visualisation%20de%20donnees/carto/web/fr/donnees/json/'.$cmd.'/'.$relations.'/'.$profondeur); // Anthony
