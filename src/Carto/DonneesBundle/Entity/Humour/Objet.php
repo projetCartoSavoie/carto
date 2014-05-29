2890a7401f7b053ad40f19a6ba1255bb35785ac3
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-05-29 09:49:35 +0200

    ajout objets

---
+++ src/Carto/DonneesBundle/Entity/Humour/Objet.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Carto\DonneesBundle\Entity\Humour;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Objet
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Carto\DonneesBundle\Entity\Humour\ObjetRepository")
+ */
+class Objet
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     * @return Objet
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Objet
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
